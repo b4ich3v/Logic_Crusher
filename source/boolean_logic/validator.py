421f27fc62cbcd22d3df7c3c02e080fc1aa96537
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-02-01 14:43:51 +0200

    Update validator.py

--- source/boolean_logic/validator.py
+++ source/boolean_logic/validator.py
@@ -1,7 +1,13 @@
 from parser_lexer.lexer import Lexer
 from parser_lexer.parser import Parser
 
+
 class Validator:
+    """
+    Provides a static method for validating whether a given Boolean expression
+    can be successfully parsed by the Lexer and Parser.
+    """
+
     @staticmethod
     def validate(expression):
         try:
